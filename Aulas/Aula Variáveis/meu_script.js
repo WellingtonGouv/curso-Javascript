29d88764a69e18e4a756f09926641e53f1e28a89
Wellington Gouveia <wellington.reguera@hotmail.com>
2021-09-08 19:49:41 -0300

    Adicionando aula de variáveis

---
+++ Aulas/Aula Variáveis/meu_script.js
@@ -0,0 +1,15 @@
+//String
+var texto = "Curso de Javascript";
+//Numver
+var numInteiro = 8;
+var numFloat = 12.75;
+//Boolean
+var isTrue = true;
+/*
+alert(texto)//abrir um dialogo
+alert(numFloat)//abrir um dialogo
+alert(numInteiro)//abrir um dialogo
+alert(isTrue)//abrir um dialogo
+*/
+//document.write(texto)
+console.log(numInteiro + "2")
