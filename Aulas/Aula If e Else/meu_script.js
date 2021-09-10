91bab8dd15f0b948e3e47bf0e2c353723d9b8447
Wellington Gouveia <wellington.reguera@hotmail.com>
2021-09-10 12:07:12 -0300

    Adicionando aula sobre operadores lógicos

--- Aulas/Aula If e Else/meu_script.js
+++ Aulas/Aula If e Else/meu_script.js
@@ -1,4 +1,4 @@
-var n1s = '15';
+/*var n1s = '15';
 var n1 = 15;
 
 if (n1s == n1){
@@ -14,4 +14,19 @@ if (n1s === n1){
 }
 else{
     document.write("Não são idênticos</br>")
+}*/
+
+//Casting de tipos
+
+var n1 = prompt("Digite a primeira nota:");
+var n2 = prompt("Digite a segunda nota:");
+
+n1 = parseFloat(n1);
+n2 = parseFloat(n2);
+
+if((n1+n2)/2 >= 5){
+    document.write("Aprovado");
+}
+else{
+    document.write("Reprovado");
 }
---
+++ Aulas/Aula Operadores Lógicos/meu_script.js
@@ -0,0 +1,25 @@
+/*if (2 == 2 && 2==3 || 45 == 9){
+    document.write("Verdadeiro");
+}
+
+else{
+    document.write("Falso");
+}
+*/
+
+var n1 = prompt("Digite a primeira nota:");
+var n2 = prompt("Digite a segunda nota:");
+var faltas = prompt("Digite o número de faltas:");
+
+n1 = parseFloat(n1);
+n2 = parseFloat(n2);
+faltas = parseInt(faltas);
+
+media = (n1+n2)/2;
+
+if(media >= 5 && faltas <=15){
+    document.write("Aprovado");
+}
+else{
+    document.write("Reprovado");
+}
