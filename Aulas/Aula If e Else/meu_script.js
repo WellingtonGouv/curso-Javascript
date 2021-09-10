5e55569acb95f2913b1d4b4db83c97946191c225
Wellington Gouveia <wellington.reguera@hotmail.com>
2021-09-10 11:53:04 -0300

    Adicionando aula sobre if/else e operadores

---
+++ Aulas/Aula If e Else/meu_script.js
@@ -0,0 +1,17 @@
+var n1s = '15';
+var n1 = 15;
+
+if (n1s == n1){
+    document.write("São iguais</br>")
+}
+
+else{
+    document.write("Não são iguais</br>")
+}
+
+if (n1s === n1){
+    document.write("São idênticos</br>")
+}
+else{
+    document.write("Não são idênticos</br>")
+}
