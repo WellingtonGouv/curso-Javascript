e41fe569dcbce6e2ab25eef1989f1db6d0740b49
Wellington Gouveia <wellington.reguera@hotmail.com>
2021-09-13 22:53:37 -0300

    Adicionando pasta de aula de operadores

---
+++ Aulas/Aula Operadores aritméticos/meu_script.js
@@ -0,0 +1,12 @@
+var num1 = 4;
+var num2 = 2;
+
+var soma = num1 + num2;
+var sub = num1 - num2;
+var div = num1 / num2;
+var mult = num1 * num2;
+
+document.write("Soma: " +soma+"</br>");
+document.write("Subtração: " +sub+"</br>");
+document.write("Divisão: " +div+"</br>");
+document.write("Multiplicação: " +mult+"</br>");
