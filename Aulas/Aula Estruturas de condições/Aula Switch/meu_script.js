8ce270511372409d61b33f1da8af592b485d6cef
Wellington Gouveia <wellington.reguera@hotmail.com>
2021-09-13 22:34:44 -0300

    Adicionando pasta para estruturas condicionais

---
+++ Aulas/Aula Estruturas de condições/Aula Switch/meu_script.js
@@ -0,0 +1,17 @@
+
+
+var opc = 3;
+
+switch(opc){
+    case 1:
+        document.write("Opc 1");
+        break;
+
+    case 2:
+        document.write("Opc 2");
+        break;
+
+    default:
+        document.write("Default");
+        break;
+}
